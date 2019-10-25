bfd4e299e746f870ebd850847dad79cc60eefa97
Dorian-Gabler <56273121+Dorian-Gabler@users.noreply.github.com>
2019-10-25 23:27:12 +0200

    Add files via upload

---
+++ mapa/sources/poland.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+echo '<iframe width="100%" height="100%" src="poland.svg" frameborder="0" scrolling="no" align="middle"></iframe>';
+}
+else
+{
+echo "not logged in";
+}
---
+++ mapa/sources/wielkopolskie.php
@@ -0,0 +1,185 @@
+﻿<?php
+session_start();
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+echo '<!DOCTYPE html>
+<html  >
+<head>
+  <!-- Site made with Mobirise Website Builder v4.11.4, https://mobirise.com -->
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="generator" content="Mobirise v4.11.4, mobirise.com">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+  <link rel="shortcut icon" href="assets/images/logo4.png" type="image/x-icon">
+  <meta name="description" content="">
+  
+  <title>Home</title>
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
+  <link rel="stylesheet" href="assets/tether/tether.min.css">
+  <link rel="stylesheet" href="assets/theme/css/style.css">
+  <link rel="preload" as="style" href="assets/mobirise/css/mbr-additional.css"><link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
+  
+  
+  
+</head>
+<body>
+  <section class="step1 cid-rFWw2Kl1pz mbr-parallax-background" id="step1-5">
+
+    
+
+    <div class="mbr-overlay" style="opacity: 0.8; background-color: rgb(239, 239, 239);">
+    </div>
+    
+    <div class="container">
+        <h2 class="mbr-section-title pb-3 mbr-fonts-style align-center display-2">
+            Poznań:</h2>
+        <h3 class="mbr-section-subtitle pb-5 mbr-fonts-style align-center display-5">&nbsp;</h3>
+        <div class="step-container">
+            <div class="card separline pb-4">
+                <div class="step-element d-flex">
+                    <div class="step-wrapper pr-3">
+                        <h3 class="step d-flex align-items-center justify-content-center">1</h3>
+                    </div>          
+                    <div class="step-text-content">
+                        <h4 class="mbr-step-title pb-3 mbr-fonts-style display-5">Piłka nożna</h4>
+                        <p class="mbr-step-text mbr-fonts-style display-7">&nbsp;Dołącz ( już wkrótce )</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card separline pb-4">
+                <div class="step-element d-flex">
+                    <div class="step-wrapper pr-3">
+                        <h3 class="step d-flex align-items-center justify-content-center">
+                            2
+                        </h3>
+                    </div>          
+                    <div class="step-text-content">
+                        <h4 class="mbr-step-title pb-3 mbr-fonts-style display-5">Siatkówka</h4>
+                        <p class="mbr-step-text mbr-fonts-style display-7">Dołącz ( już wkrótce )</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="step-element d-flex">
+                    <div class="step-wrapper pr-3">
+                        <h3 class="step d-flex align-items-center justify-content-center">
+                            3
+                        </h3>
+                    </div>          
+                    <div class="step-text-content">
+                        <h4 class="mbr-step-title pb-3 mbr-fonts-style display-5">Koszykówka</h4>
+                        <p class="mbr-step-text mbr-fonts-style display-7">
+                            Dołącz ( już wkrótce )</p>
+                    </div>
+                </div>
+            </div>
+
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+        </div>
+    </div>
+</section>
+
+<section class="engine"><a href="https://mobirise.info/l">free web templates</a></section><section class="step1 cid-rFWwmvdcee mbr-parallax-background" id="step1-6">
+
+    
+
+    <div class="mbr-overlay" style="opacity: 0.8; background-color: rgb(239, 239, 239);">
+    </div>
+    
+    <div class="container">
+        <h2 class="mbr-section-title pb-3 mbr-fonts-style align-center display-2">
+            Gorzów Wielkopolski</h2>
+        <h3 class="mbr-section-subtitle pb-5 mbr-fonts-style align-center display-5">&nbsp;</h3>
+        <div class="step-container">
+            <div class="card separline pb-4">
+                <div class="step-element d-flex">
+                    <div class="step-wrapper pr-3">
+                        <h3 class="step d-flex align-items-center justify-content-center">
+                            1
+                        </h3>
+                    </div>          
+                    <div class="step-text-content">
+                        <h4 class="mbr-step-title pb-3 mbr-fonts-style display-5">Piłka nożna</h4>
+                        <p class="mbr-step-text mbr-fonts-style display-7">
+                             Dołącz ( już wkrótce )</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card separline pb-4">
+                <div class="step-element d-flex">
+                    <div class="step-wrapper pr-3">
+                        <h3 class="step d-flex align-items-center justify-content-center">
+                            2
+                        </h3>
+                    </div>          
+                    <div class="step-text-content">
+                        <h4 class="mbr-step-title pb-3 mbr-fonts-style display-5">Siatkówka</h4>
+                        <p class="mbr-step-text mbr-fonts-style display-7">
+                             Dołącz ( już wkrótce )</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="step-element d-flex">
+                    <div class="step-wrapper pr-3">
+                        <h3 class="step d-flex align-items-center justify-content-center">
+                            3
+                        </h3>
+                    </div>          
+                    <div class="step-text-content">
+                        <h4 class="mbr-step-title pb-3 mbr-fonts-style display-5">Koszykówka
+                        </h4>
+                        <p class="mbr-step-text mbr-fonts-style display-7">
+                             Dołącz ( już wkrótce )<br><br><br><br></p>
+                    </div>
+                </div>
+            </div>
+
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+        </div>
+    </div>
+</section>
+
+
+  <script src="assets/web/assets/jquery/jquery.min.js"></script>
+  <script src="assets/popper/popper.min.js"></script>
+  <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+  <script src="assets/tether/tether.min.js"></script>
+  <script src="assets/smoothscroll/smooth-scroll.js"></script>
+  <script src="assets/parallax/jarallax.min.js"></script>
+  <script src="assets/theme/js/script.js"></script>
+  
+  
+</body>
+</html>';
+}
+else
+{
+echo "not logged in";
+}
+?>
